43b398ac27e3634a11c2747032a3c9f2dcab1939
Steven Bue <skeeve11@gmail.com>
2022-02-21 17:10:54 -0600

    Added (y/n) and (h/l) to tell player what letters to enter.

--- hi-low.py
+++ hi-low.py
@@ -18,62 +18,7 @@ YES_LIST = ['yes', 'ye', 'y']
 NO_LIST = ['no', 'n']
 
 
-def main():
-    Clear()
-    STARTING_SCORE = 300
-
-    player_score = Score(STARTING_SCORE, POINT_GAIN, POINT_LOSS)
-    game_cards = Card()
-
-    run = True
-    while run:
-
-        user_guess = ''
-        while user_guess == '':
-            display_info(is_guess_stage=True, current_card=game_cards.current_card, score=player_score.score)
-            user_guess = process_guess('Higher or Lower? ')
-            Clear()
-
-        is_correct_guess = game_cards.card_guess_check(user_guess)
-
-        if is_correct_guess:
-            player_score.add_points()
-        else:
-            player_score.remove_points()
-    
-        again = ''
-        while again == '':
-
-            if is_correct_guess:
-                print('Correct, you now have 100 more points!\n')
-            else:
-                print('Incorrect, you have lost 75 points.\n')
-
-            display_info(is_guess_stage=False, current_card=game_cards.current_card, new_card=game_cards.new_card,
-            user_guess=user_guess, score=player_score.score)
-            print()
-            
-            if not player_score.is_zero():
-                again = input('Would you like to continue? ').lower()
-            else:
-                print('Your score is Zero, you lost!')
-                sleep(3)
-
-            if again in NO_LIST or player_score.is_zero():
-                run = False
-                again = 'False'
-                Clear()
-                print('Thank you for playing!')
-                print(f'Your score: {player_score.score}')
-                sleep(3)
-            elif again not in YES_LIST:
-                again = ''
-
-        Clear()
-        game_cards.roll_new_card()
-
-
-def Clear():
+def clear():
     '''Clears the Terminal.'''
     system('cls')
 
@@ -172,5 +117,60 @@ class Card:
             return False
 
     
+
+
+def main():
+    STARTING_SCORE = 300
+
+    player_score = Score(STARTING_SCORE, POINT_GAIN, POINT_LOSS)
+    game_cards = Card()
+
+    run = True
+    while run:
+        user_guess = ''
+        while user_guess == '':
+            display_info(is_guess_stage=True, current_card=game_cards.current_card, score=player_score.score)
+            user_guess = process_guess('Next card: higher (h) or lower (l)? ')
+            clear()
+
+        is_correct_guess = game_cards.card_guess_check(user_guess)
+
+        if is_correct_guess:
+            player_score.add_points()
+        else:
+            player_score.remove_points()
+    
+        again = ''
+        while again == '':
+
+            if is_correct_guess:
+                print('Correct, you now have 100 more points!\n')
+            else:
+                print('Incorrect, you have lost 75 points.\n')
+
+            display_info(is_guess_stage=False, current_card=game_cards.current_card, new_card=game_cards.new_card,
+            user_guess=user_guess, score=player_score.score)
+            print()
+            
+            if not player_score.is_zero():
+                again = input('Would you like to continue (y/n)? ').lower()
+            else:
+                print('Your score is Zero, you lost!')
+                sleep(3)
+
+            if again in NO_LIST or player_score.is_zero():
+                run = False
+                again = 'False'
+                clear()
+                print('Thank you for playing!')
+                print(f'Your score: {player_score.score}')
+                sleep(3)
+            elif again not in YES_LIST:
+                again = ''
+
+        clear()
+        game_cards.roll_new_card()
+
+
 if __name__ == '__main__':
     main()
