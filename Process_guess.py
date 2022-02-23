7e19fc2b5baedb5f2575b9532bad352d0d7cb4c6
CanadianBleach <45185643+CanadianBleach@users.noreply.github.com>
2022-02-23 13:47:48 -0700

    Simplified the heck out of process_guess

--- Process_guess.py
+++ Process_guess.py
@@ -3,20 +3,12 @@ def process_guess(display_msg:str) -> str:
     Takes in and checks player input for incorrect input.\n
     Returns player input.
     '''
-    user_input = ''
-    try:
+    while True:
         user_input = input(f'{display_msg}').lower()
         
-        if user_input == 'h' or user_input == 'higher':
-            user_input = 'higher'
-        elif user_input == 'l' or user_input == 'lower':
-            user_input = 'lower'
+        if user_input.lower() == 'h' or user_input.lower() == 'higher':
+            return 'higher'
+        elif user_input.lower() == 'l' or user_input.lower() == 'lower':
+            return 'lower'
         else:
-            user_input = ''
-            raise ValueError
-
-    except (ValueError, KeyError):
-        print('You did not enter an accepted input, please try again.')
-        sleep(3)
-        
-    return user_input
+            print('You did not enter an accepted input, please try again.')
