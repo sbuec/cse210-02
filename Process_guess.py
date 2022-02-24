4b475abd494a0924b08771ab618e650d3e54c82f
CanadianBleach <45185643+CanadianBleach@users.noreply.github.com>
2022-02-23 17:02:13 -0700

    Fixed

--- Process_guess.py
+++ Process_guess.py
@@ -1,14 +1,23 @@
+from operator import truediv
+
+
 def process_guess(display_msg:str) -> str:
     '''
     Takes in and checks player input for incorrect input.\n
     Returns player input.
     '''
-    while True:
+    validInput = False
+    user_input = ''
+    while not validInput:
         user_input = input(f'{display_msg}').lower()
         
         if user_input.lower() == 'h' or user_input.lower() == 'higher':
-            return 'higher'
+            validInput = True
+            user_input = 'higher'
         elif user_input.lower() == 'l' or user_input.lower() == 'lower':
-            return 'lower'
+            validInput = True
+            user_input = 'lower'
         else:
             print('You did not enter an accepted input, please try again.')
+    
+    return user_input
